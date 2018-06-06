f21c432c4be2145c0e5177591549dadcee313f33
sakamoto mayu <sakamoto mayu@tsd-201601-0010>
2018-06-06 11:42:39 +0900

    sql文の修正と追加

--- sql/details.sql
+++ sql/details.sql
@@ -1,11 +1,18 @@
-create database abs4;
-
 create table details(
+	id SERIAL primary key auto_increment not null,
 	date date not null,
-	categ varchar(100) not null,
+	categ varchar(100) not null, 
 	content varchar(100),
 	cost int  not null
 );
 
-insert into details values('2018/05/30', '日用品', 'ティッシュペーパー、歯磨き粉など', -740);
-insert into details values('2018/05/30', '食費', 'ランチ', -800);
+insert into details values(1, '2018/05/30', '日用品', 'ティッシュペーパー、歯磨き粉など', -740);
+insert into details values(2, '2018/05/30', '食費', 'ランチ', -800);
+
+create table categories(
+	id SERIAL primary key auto_increment not null,
+	type varchar(100) not null
+);
+
+insert into categories values(1, '選択してください');
+insert into categories values(2, '食費');
