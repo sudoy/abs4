7f5154323885bc231d04b2d4953533c610794f9c
sudoy <yuichi.sudo@ssie.jp>
2018-06-07 13:24:30 +0900

    beansの名前を変更

---
+++ src/abs4/beans/Detail.java
@@ -0,0 +1,51 @@
+package abs4.beans;
+
+import java.time.LocalDate;
+
+public class Detail {
+
+	private LocalDate day;
+	private String type;
+	private String content;
+	private int cost;
+
+	public LocalDate getDay() {
+		return day;
+	}
+
+	public void setDay(LocalDate day) {
+		this.day = day;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public Abs4(LocalDate day, String type, String content, int cost) {
+		super();
+		this.day = day;
+		this.type = type;
+		this.content = content;
+		this.cost = cost;
+	}
+}
